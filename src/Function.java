4a5e437a55327a5ba83a1ea73d3ff47cdd9e7ff8
Pooja Tejam <pujatejam9373@gmail.com>
2024-12-08 14:51:35 +0530

    Add files via upload

---
+++ src/Function.java
@@ -0,0 +1,16 @@
+public abstract class Function {
+    Function(){
+
+    }
+
+    boolean illegalUserTextInput(String text){
+        if(text.equals("")){
+            return true;
+        }
+
+        return false;
+    }
+
+}
+
+
